7195540bfb137875617c595efc8b6b916f82d4ec
Prashant Neupane <prashantneupane8848@gmail.com>
2021-02-16 15:50:07 -0600

    Divide into functions

--- main.py
+++ main.py
@@ -4,23 +4,34 @@ import numpy as np
 
 
 img = cv2.imread("./Images/Dr.Feynman.jpg")
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img = cv2.imread("./Images/Mask.jpg")
 face_classifier = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
-face = face_classifier.detectMultiScale(img, 1.3, 5)[0]
 
-x = face[0]
-y = face[1]
-w = face[2]
-h = face[3]
+"""
+Loads the given image and finds the image from the face.
+"""
+def detect_face_in_image(img):
+    # If the given image is RGB then convert to gray to reduce complexity.
+    if len(img.shape) > 2:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        gray_img = img
 
-# (x, y, w, h) = face
-# x = x - 100
-# w = w + 100
-# y = y - 100
-# h = h + 100
+    face = face_classifier.detectMultiScale(gray_img, 1.5, 5)[0]
 
-cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    (x, y, w, h) = face
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    display_image(img, "Face Detection")
+
+"""
+Displays the image in a window with a message and closes that window after a
+key is pressed.
+"""
+def display_image(img, img_msg):
+    
+    cv2.imshow(img_msg, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+detect_face_in_image(img)
