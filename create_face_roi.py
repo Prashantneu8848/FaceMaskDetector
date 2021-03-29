3acad922e3f742c092667531b7608d2e69c8246d
Prashant Neupane <prashantneupane8848@gmail.com>
2021-03-29 15:33:15 -0500

    Connect ML and opencv pipelines

---
+++ create_face_roi.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+
+#img = cv2.imread("./Images/Dr.Feynman.jpg")
+#img = cv2.imread("./Images/Mask.jpg")
+face_classifier = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
+eyes_classifier = cv2.CascadeClassifier("./haarcascade_frontal_eyes.xml")
+
+"""
+Loads the given image and finds the image from the face.
+"""
+def detect_face_in_image(img, msg = ''):
+    # If the given image is RGB then convert to gray to reduce complexity.
+    if len(img.shape) > 2:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        gray_img = img
+    eyes = eyes_classifier.detectMultiScale(gray_img, 1.5, 5)[0]
+    face = face_classifier.detectMultiScale(gray_img, 1.5, 5)[0]
+
+    (x, y, w, h) = face
+    (x_e, y_e, w_e, h_e) = eyes
+
+    cv2.rectangle(img, (x_e, y_e), (x_e + w_e, y_e + h_e), (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x + w , y + h), (255, 0, 0), 2)
+    if msg:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, msg, (x,y), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+    display_image(img, "Face Detection With Eyes")
+
+"""
+Displays the image in a window with a message and closes that window after a
+key is pressed.
+"""
+def display_image(img, img_msg):
+    cv2.imshow(img_msg, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+"""
+Displays live video with face ROI
+"""
+def detect_face_in_video():
+    #Use the default camera.
+    video = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = video.read()
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        face = face_classifier.detectMultiScale(gray_frame, 1.1, 4)
+        eyes = eyes_classifier.detectMultiScale(gray_frame, 1.1, 4)
+
+        if len(face) != 0:
+            (x, y, w, h) = face[0]
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        if len(eyes) != 0:
+            (x_e, y_e, w_e, h_e) = eyes[0]
+
+            cv2.rectangle(frame, (x_e, y_e), (x_e + w_e, y_e + h_e), (0, 255, 0), 2)
+
+        cv2.imshow('Face Detection', frame)
+    
+        k = cv2.waitKey(1)
+        #Closes window when ESC button is pressed.
+        # Modify k = cv2.waitKey(0) & 0xFF for 64-bit machine
+        if k == 27:
+            cv2.destroyAllWindows()
+            break
+
+    video.release()
+
+#detect_face_in_image(img)
+# detect_face_in_video()
--- main.py
+++ main.py
@@ -1,76 +1,36 @@
-import tensorflow as tf
 import cv2
 import numpy as np
+import tensorflow as tf
+import tkinter as tk
+from create_face_roi import detect_face_in_image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tkinter import filedialog
 
 
-#img = cv2.imread("./Images/Dr.Feynman.jpg")
-#img = cv2.imread("./Images/Mask.jpg")
-face_classifier = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
-eyes_classifier = cv2.CascadeClassifier("./haarcascade_frontal_eyes.xml")
-
-"""
-Loads the given image and finds the image from the face.
-"""
-def detect_face_in_image(img):
-    # If the given image is RGB then convert to gray to reduce complexity.
-    if len(img.shape) > 2:
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    else:
-        gray_img = img
-    eyes = eyes_classifier.detectMultiScale(gray_img, 1.5, 5)[0]
-    face = face_classifier.detectMultiScale(gray_img, 1.5, 5)[0]
-
-    (x, y, w, h) = face
-    (x_e, y_e, w_e, h_e) = eyes
-
-    cv2.rectangle(img, (x_e, y_e), (x_e + w_e, y_e + h_e), (0, 255, 0), 2)
-    cv2.rectangle(img, (x, y), (x + w , y + h), (255, 0, 0), 2)
-    display_image(img, "Face Detection With Eyes")
-
-"""
-Displays the image in a window with a message and closes that window after a
-key is pressed.
-"""
-def display_image(img, img_msg):
-    cv2.imshow(img_msg, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-"""
-Displays live video with face ROI
-"""
-def detect_face_in_video():
-    #Use the default camera.
-    video = cv2.VideoCapture(0)
-
-    while True:
-        _, frame = video.read()
-
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        face = face_classifier.detectMultiScale(gray_frame, 1.1, 4)
-        eyes = eyes_classifier.detectMultiScale(gray_frame, 1.1, 4)
-
-        if len(face) != 0:
-            (x, y, w, h) = face[0]
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        if len(eyes) != 0:
-            (x_e, y_e, w_e, h_e) = eyes[0]
-
-            cv2.rectangle(frame, (x_e, y_e), (x_e + w_e, y_e + h_e), (0, 255, 0), 2)
-
-        cv2.imshow('Face Detection', frame)
+def prepare_image(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
     
-        k = cv2.waitKey(1)
-        #Closes window when ESC button is pressed.
-        # Modify k = cv2.waitKey(0) & 0xFF for 64-bit machine
-        if k == 27:
-            cv2.destroyAllWindows()
-            break
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return img_array_expanded_dims
 
-    video.release()
+def fetch_image_and_predict(img_path):
+    img_array_expanded_dims = prepare_image(img_path)
+    predictions = mask_classify_v1.predict(tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims))
+    result = 'wearing mask' if predictions[0][0] > predictions[0][1] else 'not wearing mask'
+    return result
 
-#detect_face_in_image(img)
-detect_face_in_video()
+mask_classify_v1 = load_model('models/mask_classify_v1.h5')
+
+root = tk.Tk()
+root.withdraw()
+
+img_path = filedialog.askopenfilename()
+
+if img_path:
+    result = fetch_image_and_predict(img_path)
+    img = cv2.imread(img_path)
+    detect_face_in_image(img, result)
+else:
+    print("user did not select any images.")
